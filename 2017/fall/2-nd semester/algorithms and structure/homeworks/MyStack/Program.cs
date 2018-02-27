3503518adeb410da98376fa6723d633323f03362
stwer1998 <stwer1998@mail.ru>
2018-02-27 19:59:29 +0300

    start

---
+++ 2017/fall/2-nd semester/algorithms and structure/homeworks/MyStack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class MyStack
+    {
+        List<int> list = new List<int>();
+        public void Push(int value)
+        {
+            list.Add(value);
+        }
+        public int Pop()
+        {
+            if (list.Count == 0) throw new InvalidOperationException();
+            var result = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return result;
+        }
+    }
+}
---
+++ 2017/fall/2-nd semester/algorithms and structure/homeworks/SortOnLexically/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortOnLexically
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public class SortOnLexically
+    {
+        public static int numberOfWords = 0;
+        public static int maximumIndex = 0;
+        public static List<string>[] pocke = new List<string>[27];
+        public static List<string> words = new List<string>();    
+        public static void Sort()
+        {
+            Max();
+            for (int i = maximumIndex - 1; i > -1; i--)
+            {
+                pocke = new List<string>[27];
+                for (int n = 0; n < pocke.Length; n++)
+                {
+                    pocke[n] = new List<string>();
+                }
+                for (int j = 0; j < numberOfWords; j++)
+                {
+                    pocke[(Convert.ToInt32((words[j][i])) - 96)].Add(words[j]);
+
+                }
+                CastPocketToList(pocke);
+            }
+        }
+        static void CastPocketToList(List<string>[] pocket)
+        {
+            words = new List<string>();
+            for (int i = 0; i < pocket.Length; i++)
+            {
+                words.AddRange(pocket[i]);
+            }
+        }
+        static void Max()
+        {
+            maximumIndex = 0;
+            foreach (var word in words)
+            {
+                if (word.Length > maximumIndex) { maximumIndex = word.Length; }
+                else continue;
+            }
+        }
+    }
+}
