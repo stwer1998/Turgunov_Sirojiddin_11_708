d69c6ed7ebfae52ce21867ab5e29687570af5ba6
stwer1998 <stwer1998@mail.ru>
2018-02-28 21:13:00 +0300

    farey

---
+++ 2017/fall/2-nd semester/algorithms and structure/homeworks/Farey/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farey.FindFarey(30);
+        }
+    }
+    class Farey
+    {
+        public static void FindFarey(int n)
+        {
+            Console.WriteLine("{0} / {1}",0,1);
+            FindFarey(n, 0, 1, 1, 1);
+            Console.WriteLine("{0} / {1}", 1, 1);
+        }
+        static void FindFarey(int n,int x,int y,int z,int t)
+        {
+            int a = x + z;
+            int b = y + t;
+            if (b <= n)
+            {
+                FindFarey(n, x, y, a, b);
+                Console.WriteLine("{0} / {1}", a,b);
+                FindFarey(n, a, b, z, t);
+            }
+        }
+    }
+}
