ad4914c6ec0921d4d85ce61ecc589dad68e6573b
stwer1998 <stwer1998@mail.ru>
2018-04-02 12:33:53 +0300

    linq

---
+++ 2017/spring/informatics/homeworks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Дана последовательность положительных целых
+           //чисел.Обрабатывая только нечетные числа, получить после -
+           //довательность их строковых представлений и отсортировать
+           //ее в лексикографическом порядке по возрастанию
+            int[] numbers = { 1, 21, 3, 43, 5, 65, 7, 91, 9 };
+            var result = numbers.Where(x => x%2==1).OrderBy(e => e.ToString()).ToArray();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
