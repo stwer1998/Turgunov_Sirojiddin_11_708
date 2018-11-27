c02ba2db64e4dd7155c3d9279c7767b24ebc0de0
stwer1998 <stwer1998@mail.ru>
2018-11-27 10:58:50 +0300

    dbservise

---
+++ 2018/misc/DataBaseApi/DataBaseApi/DataBaseWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="DataBaseWebService.asmx.cs" Class="DataBaseApi.DataBaseWebService" %>
---
+++ 2018/misc/DataBaseApi/DataBaseApi/DataBaseWebService.asmx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.Services;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace DataBaseApi
+{
+    /// <summary>
+    /// Сводное описание для DataBaseWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DataBaseWebService : WebService
+    {
+
+        public static string connectionString = ConfigurationManager.ConnectionStrings["DataBaseConnection"].ConnectionString;
+
+        [WebMethod]
+        public List<string> GetTables()
+        {
+            //no 1
+            List<string> result = new List<string>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    DataTable table = connection.GetSchema("Tables");
+                    foreach (DataRow row in table.Rows)
+                    {
+                        result.Add(row[2].ToString());
+                    }
+                }
+                catch (Exception ex) { return new List<string> { ("Что то не так") }; }
+                return result;
+            }
+        }
+        [WebMethod]
+        public List<string> GetColumn(string table)
+        {
+            if (CheckSqlInjection(table) && GetTables().Contains(table))
+            {
+                List<string> result = new List<string>();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    Console.WriteLine();
+                    var sel = ("SELECT * FROM " + table);
+                    SqlCommand command = new SqlCommand(sel, connection);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            result.Add(reader.GetName(i));
+                        }
+                    }
+                    catch (Exception ex) { return new List<string> { ("Что то не так") }; }
+                    return result;
+                }
+            }
+            else return new List<string> { ("Что то не так") };
+        }
+        [WebMethod]
+        public List<string> GetVAlues(string tablename, string condition)
+        {
+            List<string> result = new List<string>();
+            if (CheckSqlInjection(tablename) && CheckSqlInjection(condition) && GetTables().Contains(tablename))
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //var sel = "select * from Movies as m where m.Year<'1999-01-01'";
+                    var sel = "SELECT * FROM " + tablename + " WHERE " + condition;
+                    SqlCommand command = new SqlCommand(sel, connection);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                result.Add(reader[i].ToString());
+                            }
+                        }
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        return new List<string> { ("Что то не так") };
+                    }
+                    return result;
+                }
+            }
+            else return new List<string> { ("Что то не так") };
+        }
+        public static bool CheckSqlInjection(string str)
+        {
+            char a = ';';
+            char b = '\'';
+            bool c = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == a)
+                {
+                    if (c) { return true; }
+                    return false;
+                }
+                if (str[i] == b)
+                {
+                    if (c) { c = false; }
+                    else c = true;
+                }
+            }
+            return true;
+        }
+    }
+}
