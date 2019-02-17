26c83dcc377275d3cbc6e78ce5d2505ade85aed8
stwer1998 <stwer1998@mail.ru>
2019-02-17 17:14:27 +0300

    Note

---
+++ 2018/spring/Note/Note/Controllers/Methods.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Note.Controllers
+{
+    public class Methods
+    {
+        static string path = @"D:\Notes\Note\";
+        static string ending = ".txt";
+        public static List<string> GetNoteList()
+        {
+            List<string> names = new List<string>();
+            foreach (string item in Directory.GetFiles(path))
+            {
+                names.Add(item.Split(new char[] { '\\', '.' })[3]);
+            }
+            return names;
+        }
+
+        public static void DeleteNote(string name)
+        {
+            File.Delete(path + name + ending);        
+        }
+
+        public static bool CreateNote(string name, string text)
+        {
+            using (var textWriter = new StreamWriter(path + name + ending))
+            {
+                textWriter.WriteLine(text);
+            }
+            return true;
+        }
+
+        public static string GetNote(string name)
+        {
+            using (var reader = new StreamReader(path + name + ending))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
