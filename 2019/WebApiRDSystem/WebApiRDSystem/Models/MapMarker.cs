5fe90e564d9778ef867889d4acee879732175999
stwer1998 <stwer1998@mail.ru>
2019-09-28 21:19:42 +0300

    add project

---
+++ 2019/WebApiRDSystem/WebApiRDSystem/Models/MapMarker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiRDSystem.Models
+{
+    public class MapMarker
+    {
+        [Key]
+        public Guid IdMapMarker { get; private set; }
+
+        public PointOnMap Point { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ 2019/WebApiRDSystem/WebApiRDSystem/Models/PointOnMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiRDSystem.Models
+{
+    public class PointOnMap
+    {   
+        [Key]
+        public Guid IdPointOnMap { get; set; }
+
+        public float Longitude { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Heights { get; set; }
+
+        public override string ToString()
+        {
+            return Longitude + " " + Latitude + " " + Heights;
+        }
+    }
+}
